e483b4531a812343d310315838a2752e888b460d
Sidorova Tania <sidorovatania3@gmail.com>
2022-11-20 12:46:03 +0300

    Домашка 6 задача

---
+++ dom006/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+int a;
+Console.Write ("Enter the number: ");
+a = Console.Read ();
+
+if (a % 2 == 0)
+{
+   Console.WriteLine ("Even number");
+}
+
+else
+{
+    Console.WriteLine ("Odd number");
+}
+
