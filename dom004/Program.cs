ac25ace56e9df4777222225748646c73e8512cc8
Sidorova Tania <sidorovatania3@gmail.com>
2022-11-20 11:41:41 +0300

    Домашка 4 задача

---
+++ dom004/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = 22;
+int b = 3;
+int c = 9;
+int max = a;
+
+if (b > a)
+{
+   max = b;
+}
+
+ else
+ {
+    max = a;
+ }
+
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine ("max");
+Console.WriteLine (max);
+
+
+
