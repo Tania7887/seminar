27f6b95da700c4f6fd109f69bf32914a50a07e6c
Sidorova Tania <sidorovatania3@gmail.com>
2022-11-20 10:56:05 +0300

    Домашка задача 2

---
+++ dom002/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a = -0;
+int b = -3;
+int max = a;
+int min = a;
+
+if (b > a)
+
+{
+    Console.WriteLine ("Max:"); 
+    Console.WriteLine (max = b);
+    Console.WriteLine ("Min:");
+    Console.WriteLine (min = a);
+}
+
+
+if (b < a)
+
+{
+    Console.WriteLine ("Max:");
+    Console.WriteLine (max = a);
+    Console.WriteLine ("Min:");
+    Console.WriteLine (min = b);
+}
